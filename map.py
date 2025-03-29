46d8142513249a97deb41dff0506e4d95b0237ba
Kartik Tiwari <mekartik27@gmail.com>
2025-03-30 00:21:58 +0530

    textures

--- map.py
+++ map.py
@@ -1,24 +1,27 @@
 import pygame as pg
 
-_ = False
-mini_map = [
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
-    [1, _, _, _, _, _, _, _, _, _, _, _, _, _, 1],
-    [1, _, _, 1, 1, 1, _, _, _, 1, 1, _, 1, _, 1],
-    [1, _, _, 1, _, _, _, _, _, 1, _, _, 1, _, 1],
-    [1, _, _, 1, _, 1, 1, 1, 1, 1, _, 1, 1, _, 1],
-    [1, _, _, 1, _, _, _, _, _, _, _, _, 1, _, 1],
-    [1, _, _, 1, 1, 1, 1, 1, 1, 1, 1, _, _, _, 1],
-    [1, _, _, _, _, _, _, _, _, 1, _, _, 1, _, 1],
-    [1, _, _, 1, 1, _, 1, 1, _, _, _, _, _, _, 1],
-    [1, _, _, 1, _, _, _, _, _, 1, _, _, 1, _, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-]
+import random
+
+def generate_map(width, height):
+    # Initialize the map with walls (1)
+    map = [[1 for _ in range(width)] for _ in range(height)]
+
+    # Create open spaces (0) within the map
+    for i in range(1, height - 1):
+        for j in range(1, width - 1):
+            map[i][j] = 0
+
+    # Add random obstacles (1) to the map
+    for _ in range(int(width * height * 0.2)):  # 20% of the map will be obstacles
+        x, y = random.randint(1, width - 2), random.randint(1, height - 2)
+        map[y][x] = 1
+
+    return map
 
 class Map:
     def __init__(self, game):
         self.game = game
-        self.mini_map = mini_map
+        self.mini_map = generate_map(30, 20)
         self.world_map = {}
         self.get_map()
         
